e8bd1d90929c721dabcdecd19039c4b80d7c7794
Phil Crump <phildcrump@gmail.com>
2014-10-04 11:09:17 +0100

    addNode: Check if name exists first.

--- api-addNode.js
+++ api-addNode.js
@@ -18,16 +18,31 @@ app.post('/addNode', function(req, res) {
             console.log('DB Connection Error: ', err)
             return
         }
-        client.query('INSERT INTO ukhasnet.nodes (name,owner) VALUES ($1, $2)', [req.body.name, owner], function(err, result) {
-            done()
+        client.query('SELECT COUNT(*) as count FROM ukhasnet.nodes WHERE name=$1;', [req.body.name.name], function(err, result) {
             if(err) {
                 res.send(500,'Database Query Error')
                 console.log('DB Query Error: ', err)
                 return
             }
-            res.type('application/json');
-            res.set('X-Response-Time', (new Date() - startTime)+'ms');
-            res.send({'error':0})
+            var returnData={}
+            if(result.rows[0].count==0) {
+                client.query('INSERT INTO ukhasnet.nodes (name,owner) VALUES ($1, $2)', [req.body.name, owner], function(err, result) {
+                    done()
+                    if(err) {
+                        res.send(500,'Database Query Error')
+                        console.log('DB Query Error: ', err)
+                        return
+                    }
+                    res.type('application/json')
+                    res.set('X-Response-Time', (new Date() - startTime)+'ms')
+                    res.send({'error':0})
+                });
+            } else {
+                done()
+                res.type('application/json')
+                res.set('X-Response-Time', (new Date() - startTime)+'ms')
+                res.send({'error':1})
+            }
         });
     });
 });
