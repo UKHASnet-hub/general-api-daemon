8ac816db9a16663f42314b9ce87d16ce101f6cc1
Phil Crump <phildcrump@gmail.com>
2014-10-04 10:54:09 +0100

    mapNodes: Only show updated within last 14 days.

--- api-mapNodes.js
+++ api-mapNodes.js
@@ -10,7 +10,7 @@ app.get('/mapNodes', function(req, res) {
             console.log('DB Connection Error: ', err)
             return
         }
-        client.query('SELECT nodes.id AS id, nodes.name AS name, nodes.owner AS owner, nodetypes.icon AS icon, data_location.latitude AS lat, data_location.longitude AS lon, data_location.altitude AS alt FROM ukhasnet.data_location INNER JOIN ukhasnet.nodes ON nodes.locationid = data_location.id INNER JOIN ukhasnet.nodetypes ON nodetypes.id=nodes.typeid WHERE data_location.id IN (SELECT locationid FROM ukhasnet.nodes) AND nodes.typeid!=1;', function(err, result) {
+        client.query("SELECT nodes.id AS id, nodes.name AS name, nodes.owner AS owner, nodetypes.icon AS icon, data_location.latitude AS lat, data_location.longitude AS lon, data_location.altitude AS alt FROM ukhasnet.data_location INNER JOIN ukhasnet.nodes ON nodes.locationid = data_location.id INNER JOIN ukhasnet.nodetypes ON nodetypes.id=nodes.typeid INNER JOIN ukhasnet.packet_rx ON packet_rx.packetid=nodes.lastpacket WHERE data_location.id IN (SELECT locationid FROM ukhasnet.nodes) AND nodes.typeid!=1 AND packet_rx.packet_rx_time>=(NOW()-'14 day'::INTERVAL);", function(err, result) {
             done()
             if(err) {
                 res.send(500,'Database Query Error')
