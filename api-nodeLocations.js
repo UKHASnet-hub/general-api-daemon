ff61b4f3355cc2bfca5ee8ac33447116f7f2a836
Phil Crump <phil@philcrump.co.uk>
2015-05-24 00:47:39 +0100

    Add filterfix to nodeLocations.

--- api-nodeLocations.js
+++ api-nodeLocations.js
@@ -19,6 +19,10 @@ app.get('/nodeLocations', function(req, res) {
     } else {
         since_time = new Date(Date.parse(req.query.since));
     }
+    var filterfix = false
+    if(req.query.filterfix) {
+        filterfix = true
+    }
     pg.connect(pgConfig, function(err, client, done) {
         if(err) {
             res.send(500,'Database Connection Error')
@@ -33,7 +37,13 @@ app.get('/nodeLocations', function(req, res) {
             dataQuery = client.query('SELECT data_location.latitude AS lat, data_location.longitude AS lon, data_location.altitude as alt, packet_rx.packet_rx_time AS time from ukhasnet.data_location INNER JOIN (SELECT id from ukhasnet.packet AS id WHERE originid = $1) AS packet ON packet.id = data_location.packetid INNER JOIN (SELECT packetid,packet_rx_time FROM ukhasnet.packet_rx WHERE packet_rx_time >= $2) AS packet_rx on packet.id=packet_rx.packetid ORDER BY packet_rx.packet_rx_time DESC;',[req.query.id, since_time.toISOString()])
         }
         dataQuery.on('row', function(row, result) {
-            outputData.push({'lat':row.lat,'lon':row.lon,'alt':row.alt,'t':row.time})
+            if(!filterfix) {
+                outputData.push({'lat':row.lat,'lon':row.lon,'alt':row.alt,'t':row.time})
+            } else {
+                if(!(row.lat==0 && row.lat==0)) {
+                    outputData.push({'lat':row.lat,'lon':row.lon,'alt':row.alt,'t':row.time})
+                }
+            }
         })
         dataQuery.on('end', function(result) {
             done()
